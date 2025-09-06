ea543c289c700326731ef7420003d913c58dd6c9
Simon Rankine <simon@srankine.uk>
2025-09-06 21:52:42 +0000

    Test switch added to device

--- homeassistant/components/growatt_server/switch/__init__.py
+++ homeassistant/components/growatt_server/switch/__init__.py
@@ -10,10 +10,12 @@ from homeassistant.components.switch import (
     SwitchEntityDescription,
 )
 from homeassistant.config_entries import ConfigEntry
-
-# from homeassistant.const import CONF_PASSWORD, CONF_URL, CONF_USERNAME
+from homeassistant.const import CONF_PASSWORD, CONF_USERNAME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
+
+from ..const import DOMAIN
+from ..growatt import GrowattData, get_device_list, init_growatt_api
 
 
 async def async_setup_entry(
@@ -22,11 +24,20 @@ async def async_setup_entry(
     async_add_entities: AddEntitiesCallback,
 ) -> None:
     """Set up Switch Entities."""
-    # config = {**config_entry.data}
-    # username = config[CONF_USERNAME]
-    # password = config[CONF_PASSWORD]
-    # url = config.get{CONF_URL, DEFAULT_URL}
-    async_add_entities([GrowattSwitch()])
+
+    config = {**config_entry.data}
+    username = config[CONF_USERNAME]
+    password = config[CONF_PASSWORD]
+
+    api = await init_growatt_api(hass, config_entry)
+    devices, plant_id = await hass.async_add_executor_job(get_device_list, api, config)
+
+    for device in devices:
+        probe = GrowattData(
+            api, username, password, device["deviceSn"], device["deviceType"]
+        )
+        if device["deviceType"] == "mix":
+            async_add_entities([GrowattSwitch(config_entry, device, probe)])
 
 
 class GrowattSwitch(SwitchEntity):
@@ -35,16 +46,22 @@ class GrowattSwitch(SwitchEntity):
     _attr_has_entity_name = True
     _is_on = False
 
-    def __init__(self) -> None:
+    def __init__(self, config_entry: ConfigEntry, device, probe) -> None:
         """Initialize switch object."""
         super().__init__()
-        self._attr_unique_id = "growatt-test-switch"
+
+        self._attr_unique_id = f"growatt-test-switch-{config_entry.entry_id}"
         self.entity_description = SwitchEntityDescription(
             key="test",
             device_class=SwitchDeviceClass.SWITCH,
             has_entity_name=True,
             name="test",
         )
+        self._attr_device_info = {
+            "identifiers": {(DOMAIN, probe.device_id)},
+            "name": f"{device['deviceAilas']}",
+            "manufacturer": "Growatt",
+        }
 
     async def async_turn_on(self, **kwargs: Any) -> None:
         """Turn switch on."""
